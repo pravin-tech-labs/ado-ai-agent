64a76efe33903aabf0a5081d423a6d0b870061f1
Pravin <pravinpece@gmail.com>
2026-08-30 21:41:49 +0530

    Implement Git push operation

--- app.py
+++ app.py
@@ -2,6 +2,5 @@ from tools.git_tool import GitTool
 
 git_tool = GitTool()
 
-print(f"Checking Test Push")
-print(f"Staged changes: {git_tool.add_all_changes()}")
-print(f"Committed changes: {git_tool.commit_changes('Implemented commit operation')}")
+pushed_branch = git_tool.push_changes()
+print(f"Pushed branch: {pushed_branch}")
